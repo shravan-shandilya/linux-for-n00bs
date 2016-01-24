bc0f4676d7c61447468ec4c3de5b078cdab4dc98
shravanshandilya <s.shravan95@gmail.com>
2016-01-24 19:12:29 +0530

    Added hello world module and Makefile required to build it

--- linux-for-n00bs/helloworld-module/hello_module.c
+++ linux-for-n00bs/helloworld-module/hello_module.c
@@ -1,13 +1,17 @@
 #include<linux/module.h>
 #include<linux/kernel.h>
 
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Hello World");
+MODULE_AUTHOR("Shravan Shandilya");
+
 static int __init mod_init(void)
 {
 	printk(KERN_DEBUG"Hello,Module");
 	return 0;
 }
 
-static void __init mod_clean(void)
+static void __exit mod_clean(void)
 {
 	printk(KERN_DEBUG"Module removed!!");
 }
